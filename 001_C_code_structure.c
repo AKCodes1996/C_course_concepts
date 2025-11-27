037166e128fc0b87617d74d60391ae30c0cce97f
Amit Kumar <amitkumarsumit733@gmail.com>
2025-11-27 16:05:59 +0530

    added c code structure with explanation

---
+++ 001_C_code_structure.c
@@ -0,0 +1,17 @@
+//C language basic code structure concept
+
+// This is preprocessor that is providing useful input and output function to  c code
+#include<stdio.h>
+
+
+// main  is the starting point of code execution
+int main()
+{
+
+    //Here is dev code exist
+    printf("amit learning c"); // c code lines always end with semi colon 
+
+
+    // return value 0 means os understand code is correct ,always in last
+    return 0;
+}
